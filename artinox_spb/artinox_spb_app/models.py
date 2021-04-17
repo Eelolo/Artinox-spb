2aef2175f733a57c8831c35db469642e4cdbb75c
Zavmerov Nikita <NZavmerov@korusconsulting.ru>
2021-04-17 08:42:34 +0300

    new migrations, refactoring

--- artinox_spb/artinox_spb_app/models.py
+++ artinox_spb/artinox_spb_app/models.py
@@ -3,12 +3,19 @@ from django.db import models
 # Create your models here.
 
 class PageContent(models.Model):
-    title = models.CharField(max_length=255)
     block_name = models.CharField(max_length=255)
     content_text = models.TextField(blank=True)
     creation_date = models.DateField(auto_now_add=True)
     moderation_date = models.DateField(auto_now=True)
     visibility = models.BooleanField(default=True)
+    page = models.ForeignKey('PageName', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+
+class PageName(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    page_name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
